b2924859ea6adb23d94dfe9f0d3ab21941a204c3
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-20 10:54:23 -0500

    Initial Commit

---
+++ Projects/ProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectsApp.swift
+//  Projects
+//
+//  Created by Danielle Lewis on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
